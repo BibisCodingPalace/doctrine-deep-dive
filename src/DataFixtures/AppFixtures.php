bdf7f9d3b56f6dca17f3022f277c947c90cfe16b
Oliver Kossin <oliver.kossin@denkwerk.com>
2026-04-20 12:17:15 +0200

    Extend Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\TaskList;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -16,17 +17,27 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $user1 = new User('user1@symfony.com');
+        $user1 = new User('user2@example.com');
         $user1->replaceEncodedPassword(
             $this->passwordHasher->hashPassword($user1, 'user1'),
         );
         $manager->persist($user1);
 
-        $user2 = new User('user2@symfony.com');
+        $user1List = new TaskList($user1, 'Liste von user1');
+        $user1List->addItem('Ersten Task für user1 anlegen');
+        $user1List->addItem('Zweiten Task für user1 anlegen');
+        $manager->persist($user1List);
+
+        $user2 = new User('user2@example.com');
         $user2->replaceEncodedPassword(
             $this->passwordHasher->hashPassword($user2, 'user2'),
         );
         $manager->persist($user2);
+
+        $user2List = new TaskList($user2, 'Liste von user2');
+        $user2List->addItem('Ersten Task für user2 anlegen');
+        $user2List->addItem('Zweiten Task für user2 anlegen');
+        $manager->persist($user2List);
 
         $manager->flush();
     }
