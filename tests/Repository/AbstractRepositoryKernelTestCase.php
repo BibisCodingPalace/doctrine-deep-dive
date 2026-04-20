4fe34bd523ae9024ff2ae1f77c35c2760cd9e5e2
Oliver Kossin <oliver.kossin@denkwerk.com>
2026-04-20 13:25:09 +0200

    Add Queries and Tests for Task 1
    # Conflicts:
    #	src/DataFixtures/AppFixtures.php
    
    # Conflicts:
    #	Makefile
    #	src/TaskList/SummarizedTaskList.php
    #	tests/Repository/AbstractRepositoryKernelTestCase.php
    #	tests/Repository/TaskListRepositoryTest.php
    #	tests/Repository/TaskRepositoryTest.php

--- tests/Repository/AbstractRepositoryKernelTestCase.php
+++ tests/Repository/AbstractRepositoryKernelTestCase.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\Repository;
 
 use App\Tests\Support\ReloadsAppFixturesTrait;
+use App\DataFixtures\AppFixtures;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 abstract class AbstractRepositoryKernelTestCase extends KernelTestCase
 {
@@ -21,7 +24,16 @@ abstract class AbstractRepositoryKernelTestCase extends KernelTestCase
         static::reloadAppFixturesIntoDatabase();
 
         self::bootKernel();
+
         $this->entityManager = self::getContainer()->get('doctrine')->getManager();
+
+        $purger = new ORMPurger($this->entityManager);
+        $purger->purge();
+
+        $fixtures = new AppFixtures(self::getContainer()->get(UserPasswordHasherInterface::class));
+        $fixtures->load($this->entityManager);
+
+        $this->entityManager->clear();
     }
 
     protected function tearDown(): void
