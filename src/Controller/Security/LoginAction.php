e149ae31a9d337f08ac1e31b6d7815efb460b43c
Denis Brumann <dbrumann@gmail.com>
2026-04-20 12:57:12 +0200

    Enable authentication by default

--- src/Controller/Security/LoginAction.php
+++ src/Controller/Security/LoginAction.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Security;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Twig\Environment;
 
@@ -14,11 +17,17 @@ class LoginAction
     public function __construct(
         private Environment $templating,
         private AuthenticationUtils $authUtils,
+        private UrlGeneratorInterface $urlGenerator,
+        private TokenStorageInterface $userTokenStorage,
     ) {}
 
     #[Route(path: '/login', name: 'login', methods: ['GET', 'POST'])]
     public function __invoke(): Response
     {
+        if ($this->userTokenStorage->getToken() !== null) {
+            return new RedirectResponse($this->urlGenerator->generate('tasklist_list'));
+        }
+
         return new Response($this->templating->render('security/login.html.twig', [
             'error' => $this->authUtils->getLastAuthenticationError(),
             'last_username' => $this->authUtils->getLastUsername(),
